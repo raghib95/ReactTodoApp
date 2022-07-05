9d3ca0036ca54e7cf6f90fe388835f2a516762a2
Raghib Alam <raghibalam1995@gmail.com>
2022-07-05 15:08:51 +0530

    My first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Todo from './component/todoreact/todo';
+import './component/todoreact/style.css'
+
+const App = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  )
+}
+
+export default App
